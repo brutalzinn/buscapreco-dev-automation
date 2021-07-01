8e03d49299dcca6a7e1ea3deaa9d2c136543d401
=Roberto Paes <roberto.paes@al.infnet.edu.br>
2021-07-01 17:46:53 -0300

    buscapreco cli

--- request.py
+++ request.py
@@ -1,7 +1,7 @@
 import requests
 
 def loginAuth():
-    r = requests.post("http://127.0.0.1:8000/v1/user/auth",json={'email':'admin@example.com','senha':'123'})
+    r = requests.post("http://127.0.0.1:8000/v1/user/auth",json={'email':'admin@robertocpaes.dev','senha':'123'})
     print(r.status_code, r.reason)
     json_response = r.json()
     if r.status_code != 200:
